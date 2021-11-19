def43bbf0a197336ed6f98dc596cc92ddf7ef8d1
Julianna Grefkowicz <jg01386@surrey.ac.uk>
2021-11-19 16:34:44 +0000

    Added testing of the home page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'Janusz A. Zajdel Award'
+    assert_select 'h1', 'Janusz A. Zajdel Award'
+    assert_select 'p', 'Welcome to the Zajdel Award English page!'
   end
 
 end
