908d044f8f7f52d217bc3803e448c25dff015b6e
Julianna Grefkowicz <jg01386@surrey.ac.uk>
2021-12-07 10:44:12 +0000

    Added dark mode and validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,59 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  if(document.getElementById('AuthorForm'))
+  {
+    ValidateAuthorForm();
+  }
+
+  if(document.getElementById('WorkForm'))
+  {
+    ValidateWorkForm();
+  }
+});
+
+function  ValidateAuthorForm()
+{
+    $('#AuthorForm').validate({
+      rules: {
+        'author[first_name]': {required: true},
+        'author[surname]': {required: true},
+      },
+      messages: {
+        'author[first_name]': {required: "You must enter author's first name",},
+        'author[surname]': {required: "You must enter author's surname",},
+      }
+    });
+}
+
+function  ValidateWorkForm()
+{
+    $('#WorkForm').validate({
+      rules: {
+        'literary_work[title]': {required: true},
+        'literary_work[category]': {required: true},
+        'literary_work[nominated_year]': {
+          required: true,
+          digits: true,
+          range: [1950, 2100],
+        },
+        'literary_work[has_won]': {required: true}
+      },
+      messages: {
+        'literary_work[title]': {required: "You must enter the title",},
+        'literary_work[category]': {required: "You must enter the category",},
+        'literary_work[nominated_year]': {
+          required: "The year of nomination is required",
+          digits: "The year should be a whole year between 1950 and 2100",
+          range: "The year should be a whole year between 1950 and 2100",
+        },
+        'literary_work[has_won]': {required: "The has_won fiels is required"}
+      }
+    });
+}
+
+function DarkMode(val) {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
